3abd7d22ab6ae480ed5d6f763ae04239eefd28d2
Garbii1 <muhdgaruba@gmail.com>
2025-04-14 21:08:40 +0100

    Configure frontend for production build and deployment

--- telemed-ui/src/environments/environment.prod.ts
+++ telemed-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // --- IMPORTANT FOR DEPLOYMENT ---
-    apiUrl: 'https://yourusername.pythonanywhere.com/api' // Your deployed PythonAnywhere backend URL
+    apiUrl: 'https://garbimuhd.pythonanywhere.com/api' // Your deployed PythonAnywhere backend URL
   };
